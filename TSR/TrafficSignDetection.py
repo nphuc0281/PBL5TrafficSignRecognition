bad809154ed608738f6f1940251b33a960bb22fa
nphuc0281 <nphuc0281@gmail.com>
2022-06-23 00:09:00 +0700

    Improve regconition

--- TSR/TrafficSignDetection.py
+++ TSR/TrafficSignDetection.py
@@ -4,10 +4,6 @@ from .Camera import Camera
 
 
 class TrafficSignDetection:
-    """
-    Class implement YOLOv5 model to detect traffic sign from camera
-    """
-
     def __init__(self, out_file, path_to_weight, cam_mode=None):
         self.model = self.load_model(path_to_weight)
         self.classes = self.model.names
@@ -24,11 +20,6 @@ class TrafficSignDetection:
         return model
 
     def score_frame(self, frame):
-        """
-        Takes a single frame as input, and scores the frame using yolo5 model.
-        :param frame: input frame in numpy/list/tuple format.
-        :return: Labels and Coordinates of objects detected by model in the frame.
-        """
         self.model.to(self.device)
         frame = [frame]
         results = self.model(frame)
@@ -36,20 +27,9 @@ class TrafficSignDetection:
         return labels, cord
 
     def class_to_label(self, x):
-        """
-        For a given label value, return corresponding string label
-        :param x: numeric label
-        :return: corresponding string label
-        """
         return self.classes[int(x)]
 
     def plot_boxes(self, results, frame):
-        """
-        Takes a frame and its results as input, and plots the bounding boxes and label on to the frame.
-        :param results: contains labels and coordinates predicted by model on the given frame.
-        :param frame: Frame which has been scored.
-        :return: Frame with bounding boxes and labels ploted on it.
-        """
         labels, cord = results
         n = len(labels)
         x_shape, y_shape = frame.shape[1], frame.shape[0]
@@ -60,6 +40,6 @@ class TrafficSignDetection:
                 x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
                 bgr = (0, 255, 0)
                 cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
-                cv2.putText(frame, self.class_to_label(labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
+                cv2.putText(frame, self.class_to_label(labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.4, bgr, 2)
 
         return frame
--- main.py
+++ main.py
@@ -2,60 +2,98 @@ from TSR import TrafficSignDetection
 from time import time
 import numpy as np
 import cv2
-import sv_ttk
 from tkinter import *
 from tkinter.ttk import *
 import tkinter
 import PIL.Image
 import PIL.ImageTk
+from multiprocessing.pool import ThreadPool
 
 
 class GUI(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
         self.parent = parent
-        self.core = TrafficSignDetection(out_file='vid.avi', path_to_weight='./models/TSR.pt', cam_mode=1)
+        self.core = TrafficSignDetection(out_file='vid.avi', path_to_weight='./models/TSR.pt')
 
         # Variables
         self.camera_image = None
         self.after_id = None
+        self.ts_labels = {
+            'CamDiNguocChieu': 'Cấm đi ngược chiều',
+            'CamDo': 'Cấm đỗ',
+            'CamQuayDau': 'Cấm quay đầu',
+            'CamReTrai': 'Cấm rẽ trái',
+            'CamRePhai': 'Cấm rẽ phải',
+            'CamBopCoi': 'Cấm bóp còi',
+            'GiaoNhauVoiDuongUuTien': 'Giao nhau với đường ưu tiên',
+            'GiaoNhauVoiDuongKhongUuTien': 'Giao nhau với đường không ưu tiên',
+            'GiaoNhauChayTheoVongXuyen': 'Giao nhau chạy theo vòng xuyến',
+            'GiaoNhauVoiDuongSatCoRaoChan': 'Giao nhau với đường sắt có rào chắn',
+            'TreEmQuaDuong': 'Trẻ em qua đường',
+            'BenhVien': 'Bệnh viện',
+        }
 
         # Window
         self.parent.title("Traffic Sign Recognition")
-        self.parent.geometry("680x600")
+        self.parent.geometry("400x400")
 
         # Camera view
-        canvas_w = self.core.camera.get(cv2.CAP_PROP_FRAME_WIDTH)
-        canvas_h = self.core.camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        canvas_w = self.core.camera.get(cv2.CAP_PROP_FRAME_WIDTH) // 2
+        canvas_h = self.core.camera.get(cv2.CAP_PROP_FRAME_HEIGHT) // 2
         self.camera_canvas = Canvas(self.parent, width=canvas_w, height=canvas_h, bg="grey")
-        self.camera_canvas.pack(pady=(20,0))
+        self.camera_canvas.pack(side='top',pady=(20, 0), expand=True)
 
         # Button start camera
         self.button_start = Button(windows, text="Start", command=self.start_camera)
-        self.button_start.pack(side='left', anchor='e', expand=True, padx=(0,10))
+        self.button_start.pack(side='left', anchor='e', expand=True, padx=(0, 10), pady=(0, 20))
 
         # Button stop camera
         self.button_stop = Button(windows, text="Stop", command=self.stop_camera)
-        self.button_stop.pack(side='right', anchor='w', expand=True)
+        self.button_stop.pack(side='right', anchor='w', expand=True, pady=(0, 20))
+
+        # Label
+        self.lblResults = Label(windows, text="No traffic sign detected.", foreground='red')
+        self.lblResults.pack(side='bottom', anchor='s', expand=True, pady=(0, 20))
 
     def start_camera(self):
-        start_time = time()
-
         # Read from camera
-        ret, frame = self.core.camera.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        _, frame = self.core.camera.read()
 
         # Processing frame with YOLOv5 model and labeling
-        results = self.core.score_frame(frame)
-        frame = self.core.plot_boxes(results, frame)
-        end_time = time()
-        fps = 1/np.round(end_time - start_time, 3)
-        print(f"Frames Per Second : {fps}")
+        pool = ThreadPool(processes=1)
+        async_result = pool.apply_async(self.recognition_process, (frame, ))  # tuple of args for foo
+        frame, results = async_result.get()
+
+        # Change labels
+        labels, _ = results
+        labels = [self.ts_labels[self.core.class_to_label(labels[i])] for i in range(len(labels))]
+        if len(labels) > 1:
+            label = '\n'.join(labels)
+        elif len(labels) == 1:
+            label = labels[0]
+        else:
+            label = 'No traffic sign detected.'
+        self.lblResults['text'] = label
+
 
         # Show camera
         self.camera_image = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
         self.camera_canvas.create_image(0, 0, image=self.camera_image, anchor=tkinter.NW)
         self.after_id = self.parent.after(15, self.start_camera)
+
+    def recognition_process(self, frame):
+        start_time = time()
+        height, width, _ = frame.shape
+        frame = cv2.resize(frame, (width // 2, height // 2))
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = self.core.score_frame(frame)
+        frame = self.core.plot_boxes(results, frame)
+        end_time = time()
+        fps = 1 / np.round(end_time - start_time, 3)
+        print(f"Frames Per Second : {fps}")
+        print(end_time - start_time)
+        return frame, results
 
     def stop_camera(self):
         if self.after_id:
@@ -65,7 +103,6 @@ class GUI(Frame):
 
 # Start app GUI
 windows = Tk()
-sv_ttk.set_theme("light")
 app = GUI(windows)
 windows.mainloop()
 
